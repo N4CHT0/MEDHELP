d9560cf4ee13a93f0c9c1dba3eb768c99392e5cf
N4CHT0 <hannessinaga77@gmail.com>
2023-12-06 04:47:03 +0700

    [BAB 6] Animation

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,14 +1,28 @@
-import { Image, ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Image,Animated, ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, {useRef} from 'react'
 import {Call, ChartCircle,Hospital,Notification, People, Setting} from 'iconsax-react-native';
 import { fontType } from '../../theme';
 import { SLIDER } from '../../assets';
+import { useNavigation,useFocusEffect } from "@react-navigation/native";
 import FastImage from 'react-native-fast-image'
 const Services = () => {
+  const navigation = useNavigation();
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const diffClampY = Animated.diffClamp(scrollY, 0, 142);
+  const recentY = diffClampY.interpolate({
+      inputRange: [0, 142],
+      outputRange: [0, -142],
+      extrapolate: 'clamp',
+    });
   return (
-    <View>
-      <ScrollView>
-        <View style={styles.header}>
+    <View style={{flex: 1}}>
+      <Animated.ScrollView
+      onScroll={Animated.event(
+        [{nativeEvent: {contentOffset: {y: scrollY}}}],
+        {useNativeDriver: true},
+      )}
+      contentContainerStyle={{paddingTop: 0}}>
+        <Animated.View style={[styles.header, {transform: [{translateY: recentY}]}]}>
             <ChartCircle size="36" color="#1F91FC"/>
             <Text style={styles.headerTitle}>MEDHELP.</Text>
             <TouchableOpacity>
@@ -21,7 +35,7 @@ const Services = () => {
                 <Setting size="30" color="#232425"/>
               </View>
             </TouchableOpacity>
-        </View>
+        </Animated.View>
         <View style={{flexDirection: 'row',marginHorizontal: 20,gap: 20}}>
           <TouchableOpacity>
             <ImageBackground source={SLIDER} style={{padding: 30,alignItems: 'center', gap: 10}} imageStyle={{borderRadius: 20,}}>
@@ -40,7 +54,52 @@ const Services = () => {
           <View>
             <Text style={{fontFamily: fontType['NS-Bold'], fontSize: 18,}}>Boost Your Immunity</Text>
           </View>
-          <View>
+          <View style={{gap: 20}}>
+            <TouchableOpacity>
+              <View style={{flexDirection: 'row', gap: 20,}}>
+                <FastImage style={{width: 100,height: 100}} source={{uri : 'https://s3-v2-klikdokter-marketplace.s3.ap-southeast-1.amazonaws.com/m0cha-uuxahp2xiP/uploads/product/17928/17928_1637116854.2746.jpg'}}/>
+                <View style={{flexWrap: 'wrap', flexDirection: 'column'}}>
+                  <Text style={{fontFamily: fontType['NS-Light']}}>Imboost Vitamin C Tube 8 Tablet</Text>
+                  <Text style={{fontFamily: fontType['NS-SemiBold']}}>Rp.42.000.00</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={{flexDirection: 'row', gap: 20,}}>
+                <FastImage style={{width: 100,height: 100}} source={{uri : 'https://s3-v2-klikdokter-marketplace.s3.ap-southeast-1.amazonaws.com/m0cha-uuxahp2xiP/uploads/product/17928/17928_1637116854.2746.jpg'}}/>
+                <View style={{flexWrap: 'wrap', flexDirection: 'column'}}>
+                  <Text style={{fontFamily: fontType['NS-Light']}}>Imboost Vitamin C Tube 8 Tablet</Text>
+                  <Text style={{fontFamily: fontType['NS-SemiBold']}}>Rp.42.000.00</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={{flexDirection: 'row', gap: 20,}}>
+                <FastImage style={{width: 100,height: 100}} source={{uri : 'https://s3-v2-klikdokter-marketplace.s3.ap-southeast-1.amazonaws.com/m0cha-uuxahp2xiP/uploads/product/17928/17928_1637116854.2746.jpg'}}/>
+                <View style={{flexWrap: 'wrap', flexDirection: 'column'}}>
+                  <Text style={{fontFamily: fontType['NS-Light']}}>Imboost Vitamin C Tube 8 Tablet</Text>
+                  <Text style={{fontFamily: fontType['NS-SemiBold']}}>Rp.42.000.00</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={{flexDirection: 'row', gap: 20,}}>
+                <FastImage style={{width: 100,height: 100}} source={{uri : 'https://s3-v2-klikdokter-marketplace.s3.ap-southeast-1.amazonaws.com/m0cha-uuxahp2xiP/uploads/product/17928/17928_1637116854.2746.jpg'}}/>
+                <View style={{flexWrap: 'wrap', flexDirection: 'column'}}>
+                  <Text style={{fontFamily: fontType['NS-Light']}}>Imboost Vitamin C Tube 8 Tablet</Text>
+                  <Text style={{fontFamily: fontType['NS-SemiBold']}}>Rp.42.000.00</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={{flexDirection: 'row', gap: 20,}}>
+                <FastImage style={{width: 100,height: 100}} source={{uri : 'https://s3-v2-klikdokter-marketplace.s3.ap-southeast-1.amazonaws.com/m0cha-uuxahp2xiP/uploads/product/17928/17928_1637116854.2746.jpg'}}/>
+                <View style={{flexWrap: 'wrap', flexDirection: 'column'}}>
+                  <Text style={{fontFamily: fontType['NS-Light']}}>Imboost Vitamin C Tube 8 Tablet</Text>
+                  <Text style={{fontFamily: fontType['NS-SemiBold']}}>Rp.42.000.00</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
             <TouchableOpacity>
               <View style={{flexDirection: 'row', gap: 20,}}>
                 <FastImage style={{width: 100,height: 100}} source={{uri : 'https://s3-v2-klikdokter-marketplace.s3.ap-southeast-1.amazonaws.com/m0cha-uuxahp2xiP/uploads/product/17928/17928_1637116854.2746.jpg'}}/>
@@ -52,7 +111,7 @@ const Services = () => {
             </TouchableOpacity>
         </View>
           </View>
-      </ScrollView>
+      </Animated.ScrollView>
     </View>
   )
 }
